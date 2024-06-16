47d5926cb7740ef89f3b492cbe5b2bcd3e164555
leasalikov <144378042+leasalikov@users.noreply.github.com>
2024-06-16 15:59:44 +0300

    Delete client directory

--- client/src/components/Volunteer.jsx
+++
@@ -1,8 +0,0 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { useContext } from "react";
-import { UserContext } from '../App';
-const Register = () => {
-    
-};
-export default Register;
