04354df9be8bab88d7b23d50e6f4bf372eb50a9f
leasalikov <leasalikov@gmail.com>
2024-06-30 21:33:07 +0300

    ghdfgj

--- server/service/needyService.js
+++ server/service/needyService.js
@@ -16,12 +16,14 @@ export class NeedyService {
         console.log("tableName")
         // const keys = Object.keys(sortByObj);
         const values = Object.values(sortByObj);
-        const query = getByQuery5();   //check if user exist in the needies
+        const query = getByQuery6("needies","usernameneedies","idneedies");   //check if user exist in the needies
         console.log("query: ", query)
         result = await executeQuery(query, values);
         console.log("result getBy: ", result)
         return result;
     }
+
+    
 
     async getneedycategory(sortByObj, tablename, column) {
         var result;
@@ -30,80 +32,123 @@ export class NeedyService {
         const values = Object.values(sortByObj);
         console.log("valeus", values)
         const key = Object.keys(sortByObj);
-        const query = getByQuery9(tablename, key, column);        //check if user exist in the volunteers
+        const query = getByQuery9(tablename, key, column);        //check if user exist in the needys
         console.log("query: ", query)
         result = await executeQuery(query, values);
         console.log("result getBy: ", result)
         return result;
     }
 
-    async addNeedy(needies, needyItem) {
-        const idcategory = [];
-        let result3;
-        let query3;
-        //gets the NeedyIdArray
-        const categoryArray = needyItem.namecategory;
-        console.log("ca===========  ", categoryArray)
-        // if(categoryArray.length == 0){
-        //     alert("לא בוצעה בחירה, נא לבחור פריט מהרשימה")
-        // }
-        for (let element = 0; element <= categoryArray.length - 1; element++) {
-            query3 = getByQuery("category", ["namecategory"]);
-            result3 = await executeQuery(query3, [categoryArray[element]]);
-            idcategory.push(result3[0].idcategory)
-            console.log("idcategory ", idcategory[element])
+    async getcategory(NeedyItem) {
+        let result, query;
+        const idcategoryArray = [];
+        const categoryArrayName = NeedyItem.namecategory;
+        for (let element = 0; element <= categoryArrayName.length - 1; element++) {
+            query = getByQuery("category", ["namecategory"]);
+            result = await executeQuery(query, [categoryArrayName[element]]);
+            idcategoryArray.push(result[0].idcategory)
         };
-        // console.log("needyItem ", needyItem);
-        delete needyItem.namecategory;
-        console.log("needyItem ", needyItem)
+        return idcategoryArray;
+    }
 
+    async addneedy(needys, needyItem) {
+
+        //gets the categoryIdArray
+        const idcategoryArray = await this.getcategory(needyItem)
+        delete needyItem.namecategory;
         // add the user to needy
-        const result = await this.getBy(needyItem)
-        console.log("resultfeht", result)
-        var result1;
-        // console.log(result[0]["COUNT(*)"])
-        var id;
-        if (result.length == 0) {
-            console.log("count = 0")
+        const result = await this.getneedyBy(needyItem, "needys", "idneedys")//check if user exist
+        var result1, id;
+        if (result.length == 0) {//if needy is not exist
             const values = Object.values(needyItem)
             const keys = Object.keys(needyItem);
-            const query = addQuery(needies, keys);
+            const query = addQuery(needys, keys);
             result1 = await executeQuery(query, values);
             id = result1.insertId;
         }
-        else {
+        else {//else, gets the data of needy
             const values = Object.values(needyItem)
-            const query = getByQuery5();
+            const query = getByQuery6("needys", "usernamevolenteers", "*");////
+            console.log(query)
             result1 = await executeQuery(query, values);
-            console.log("result1: ", result1)
-            id = result1[0].idneedies;
-            // console.log()
+            id = result1[0].idneedys;
         }
+        //add to needyCategory
+        const addneedycategory = await this.addneedycategory(idcategoryArray, id)
+    //    let idneedies = addneedycategory.idneedies
+    //     let idcategoryarray = addneedycategory.idcategoryarray
 
-        console.log("idcat " + idcategory);
-        console.log("resultid:", id)
-        // add the user to categoryneedies
-        let objects;
-        let values2;
-        let keys2;
-        let query2;
-        let result2 = [];
-        console.log("adddddddddddddddddd")
-        for (let element = 0; element <= categoryArray.length - 1; element++) {
-            objects = { "idneedies": id, "idcategory": idcategory[element] }
-            values2 = Object.values(objects)
-            console.log("v2" + values2)
-            keys2 = Object.keys(objects)
-            console.log("v2 ", values2)////
-            console.log("k2 ", keys2)
-            query2 = addQuery("categoryneedies", keys2);
-            console.log("query2 ", query2)
-            result2.push(await executeQuery(query2, values2));
-            console.log("result2" + result2[element])
-        }
-        delete needyItem.categoryArray;
-        return { result1, result2 }
+        return { result1,idcategoryArray,needyItem }
     }
+
+
+    // async addNeedy(needies, needyItem) {
+    //     const idcategory = [];
+    //     let result3;
+    //     let query3;
+    //     //gets the NeedyIdArray
+    //     const categoryArray = needyItem.namecategory;
+    //     console.log("ca===========  ", categoryArray)
+    //     // if(categoryArray.length == 0){
+    //     //     alert("לא בוצעה בחירה, נא לבחור פריט מהרשימה")
+    //     // }
+    //     for (let element = 0; element <= categoryArray.length - 1; element++) {
+    //         query3 = getByQuery("category", ["namecategory"]);
+    //         result3 = await executeQuery(query3, [categoryArray[element]]);
+    //         idcategory.push(result3[0].idcategory)
+    //         console.log("idcategory ", idcategory[element])
+    //     };
+    //     // console.log("needyItem ", needyItem);
+    //     delete needyItem.namecategory;
+    //     console.log("needyItem ", needyItem)
+
+    //     // add the user to needy
+    //     const result = await this.getBy(needyItem)
+    //     console.log("resultfeht", result)
+    //     var result1;
+    //     // console.log(result[0]["COUNT(*)"])
+    //     var id;
+    //     if (result.length == 0) {
+    //         console.log("count = 0")
+    //         const values = Object.values(needyItem)
+    //         const keys = Object.keys(needyItem);
+    //         const query = addQuery(needies, keys);
+    //         result1 = await executeQuery(query, values);
+    //         id = result1.insertId;
+    //     }
+    //     else {
+    //         const values = Object.values(needyItem)
+    //         const query = getByQuery5();
+    //         result1 = await executeQuery(query, values);
+    //         console.log("result1: ", result1)
+    //         id = result1[0].idneedies;
+    //         // console.log()
+    //     }
+
+    //     console.log("idcat " + idcategory);
+    //     console.log("resultid:", id)
+    //     // add the user to categoryneedies
+    //     let objects;
+    //     let values2;
+    //     let keys2;
+    //     let query2;
+    //     let result2 = [];
+    //     console.log("adddddddddddddddddd")
+    //     for (let element = 0; element <= categoryArray.length - 1; element++) {
+    //         objects = { "idneedies": id, "idcategory": idcategory[element] }
+    //         values2 = Object.values(objects)
+    //         console.log("v2" + values2)
+    //         keys2 = Object.keys(objects)
+    //         console.log("v2 ", values2)////
+    //         console.log("k2 ", keys2)
+    //         query2 = addQuery("categoryneedies", keys2);
+    //         console.log("query2 ", query2)
+    //         result2.push(await executeQuery(query2, values2));
+    //         console.log("result2" + result2[element])
+    //     }
+    //     delete needyItem.categoryArray;
+    //     return { result1, result2 }
+    // }
 
     async update(tableName, needyItem, id) {
         const keys = Object.keys(needyItem);
@@ -123,7 +168,7 @@ export class NeedyService {
         return await executeQuery(query, [numOfLimit, startLimit]);
     }
 
-    async getNeedyByVolunteer(idcategoryArray) {
+    async getNeedyByneedy(idcategoryArray) {
 
         console.log("idcategogfrjtyryArray", idcategoryArray)
         // idcategoryArray=idcategoryArray.idcategoryneedy
