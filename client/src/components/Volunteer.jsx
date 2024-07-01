7a8e4f36a8c947df04da3f1a0f87b8ff7921064d
leasalikov <leasalikov@gmail.com>
2024-07-01 15:17:21 +0300

    ,jmhn

--- client/src/components/Volunteer.jsx
+++ client/src/components/Volunteer.jsx
@@ -59,39 +59,40 @@ async function Volunteer() {
 
     return (
         <>
-        <div>
-            {/* <Header /> */}
-            {/* {showEndMassage &&
-                <div>
-                    <h2>!תודה ותזכו למצוות<br />כעת ישלח אליך מייל עם פרטי המזמין ליצירת קשר</h2>
-                    <button onClick={addVolunting}>להוספת התנדבות</button>
-                </div>}
-            {showComponent && <div>
-                <h1>שלום {currentUser.username}!!</h1> */}
-                <h2>מעריכים אותך על נכונותך לעזור ולקחת חלק, לפניך האפשרויות הרלוונטיות בהתאם לבחירתך.</h2>
-                <table>
-                    <thead>
-                        <tr>
-                            <th>name</th>
-                            <th>email</th>
-                            <th>phone</th>
-                            <th>category</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {location.state.data.map((item, i) => (
-                            <tr key={i}>
-                                <td>{item[0].username}</td>
-                                <td>{item[0].email}</td>
-                                <td>{item[0].phone}</td>
-                                <td>{item.namecategory[0].namecategory}</td>
-                                {/* <td>{item[0].idUser}</td> */}
-                                <td><button onClick={() => linking(item)}>V</button></td>
+            <div>
+                <Header />
+                {showEndMassage &&
+                    <div>
+                        <h2>!תודה ותזכו למצוות<br />כעת ישלח אליך מייל עם פרטי המזמין ליצירת קשר</h2>
+                        <button onClick={addVolunting}>להוספת התנדבות</button>
+                    </div>}
+                {showComponent && <div>
+                    <h1>שלום {currentUser.username}!!</h1>
+                    <h2>מעריכים אותך על נכונותך לעזור ולקחת חלק, לפניך האפשרויות הרלוונטיות בהתאם לבחירתך.</h2>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>name</th>
+                                <th>email</th>
+                                <th>phone</th>
+                                <th>category</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>}
+                        </thead>
+                        <tbody>
+                            {location.state.data.map((item, i) => (
+                                <tr key={i}>
+                                    <td>{item[0].username}</td>
+                                    <td>{item[0].email}</td>
+                                    <td>{item[0].phone}</td>
+                                    <td>{item.namecategory[0].namecategory}</td>
+                                    {/* <td>{item[0].idUser}</td> */}
+                                    <td><button onClick={() => linking(item)}>V</button></td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>}
+            </div>
         </>
     )
 };
