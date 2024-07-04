dcdd1744dcad2c8bef269ec4fca3973a46ed4e27
Lea Salikov <Hand4c18@mbjcomp.org.il>
2024-07-04 23:52:47 +0300

    cs

--- server/controllers/emailController.js
+++ server/controllers/emailController.js
@@ -1,24 +1,24 @@
 
-import { EmailService } from '../service/emailService'
-// const loginTry = [];
+// import { EmailService } from '../service/emailService'
+// // const loginTry = [];
 
-export class EmailController {
-    async postemail(req, res, next) {
-        try {
-            const emailService = new EmailService();
-            const resualt = await emailService.getemail(req, res, next);
-            if (resualt == undefined)
-                return res.status(401).json({ resualt: "emailName duplicate" });
+// export class EmailController {
+//     async postemail(req, res, next) {
+//         try {
+//             const emailService = new EmailService();
+//             const resualt = await emailService.getemail(req, res, next);
+//             if (resualt == undefined)
+//                 return res.status(401).json({ resualt: "emailName duplicate" });
 
-            res.status(200).json(resualt);
-        }
-        catch (ex) {
-            const err = {};
-            err.statusCode = 500;
-            err.message = ex;
-            next(err);
-        }
+//             res.status(200).json(resualt);
+//         }
+//         catch (ex) {
+//             const err = {};
+//             err.statusCode = 500;
+//             err.message = ex;
+//             next(err);
+//         }
         
-    }
+//     }
 
-}
+// }
--- server/service/emailService.js
+++ server/service/emailService.js
@@ -1,44 +1,44 @@
 
-// import { executeQuery } from './db.js';
-// import { getQuery, getByQuery, deleteQuery, addQuery, updateQuery, getByQuery2, getByQuery3, getByQuery4, getByQuery7, getByQuery6, getByQuery9 } from './query.js'
-// let nodemailer = require('nodemailer');
-import nodemailer from 'nodemailer'
+// // import { executeQuery } from './db.js';
+// // import { getQuery, getByQuery, deleteQuery, addQuery, updateQuery, getByQuery2, getByQuery3, getByQuery4, getByQuery7, getByQuery6, getByQuery9 } from './query.js'
+// // let nodemailer = require('nodemailer');
+// import nodemailer from 'nodemailer'
 
 
 
-export class EmailService {
-    async sendEmail(req, res, next) {
-        try {
-            console.log()
-            var transporter = nodemailer.createTransport({
-                service: 'outlook',
-                auth: {
-                    user: 'ScooterService@outlook.co.il',
-                    pass: 'ServiceScooter'
-                }
-            });
-            var mailOptions = {
-                from: 'ScooterService@outlook.co.il',
-                to: JSON.stringify(data1.Email),
-                subject: `order pass ${parseInt(data1.Password)}`,
-                html: `<div font-weight: 900; font-size: larger; font-family: inherit;">
-                <h1>hello </h1>
-                <br/>
-                <p>you ordered succesfully!!!!</p>
-            </div>`
-            };
+// export class EmailService {
+//     async sendEmail(req, res, next) {
+//         try {
+//             console.log()
+//             var transporter = nodemailer.createTransport({
+//                 service: 'outlook',
+//                 auth: {
+//                     user: 'ScooterService@outlook.co.il',
+//                     pass: 'ServiceScooter'
+//                 }
+//             });
+//             var mailOptions = {
+//                 from: 'ScooterService@outlook.co.il',
+//                 to: JSON.stringify(data1.Email),
+//                 subject: `order pass ${parseInt(data1.Password)}`,
+//                 html: `<div font-weight: 900; font-size: larger; font-family: inherit;">
+//                 <h1>hello </h1>
+//                 <br/>
+//                 <p>you ordered succesfully!!!!</p>
+//             </div>`
+//             };
 
-            transporter.sendMail(mailOptions, function (error, info) {
-                if (error) {
-                    throw new Error("Can't send a mail!")
-                } else {
-                    console.log('Email sent: ' + info.response);
-                }
-            });
-        }
-        catch (err) {
-            console.log("err sendEmail1" + err);
-        }
-    }
-}
+//             transporter.sendMail(mailOptions, function (error, info) {
+//                 if (error) {
+//                     throw new Error("Can't send a mail!")
+//                 } else {
+//                     console.log('Email sent: ' + info.response);
+//                 }
+//             });
+//         }
+//         catch (err) {
+//             console.log("err sendEmail1" + err);
+//         }
+//     }
+// }
 
