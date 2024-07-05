de647c6e8a7b594bf47c0692a3bea8526d0b0d72
Lea Salikov <Hand4c18@mbjcomp.org.il>
2024-07-05 17:35:28 +0300

    vdfs

--- client/src/components/GoogleMap.jsx
+++ client/src/components/GoogleMap.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import 'leaflet/dist/leaflet.css';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import axios from 'axios';
